24d6f129295bb37924849ffa0e319f72f6da00bd
RahafN1 <rahafalabdalh24@gmail.com>
2026-06-13 18:02:02 +0300

    Handle scalar input error message for lazy_matrix_mul

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -9,4 +9,8 @@ def lazy_matrix_mul(m_a, m_b):
     """
     Multiplies two matrices using NumPy.
     """
-    return np.matmul(m_a, m_b)
+    a = np.array(m_a)
+    b = np.array(m_b)
+    if a.ndim == 0 or b.ndim == 0:
+        raise ValueError("Scalar operands are not allowed, use '*' instead")
+    return np.matmul(a, b)
