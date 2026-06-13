38465e33deaff7a97c0edc7e3b94a9e6f80c6474
RahafN1 <rahafalabdalh24@gmail.com>
2026-06-13 18:18:18 +0300

    Use print with end to avoid extra newline

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,30 +1,22 @@
 #!/usr/bin/python3
-"""
-Module that prints text with 2 new lines after ., ? and :
-"""
+"""Print text with 2 new lines after '.', '?' and ':'"""
 
 
 def text_indentation(text):
-    """
-    Prints a text with 2 new lines after each ., ? and :
+    """Print text with 2 new lines after '.', '?' and ':'"""
 
-    Args:
-        text: the text to print
-
-    Raises:
-        TypeError: if text is not a string
-    """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    n = len(text)
-    i = 0
-    while i < n:
-        char = text[i]
-        if char == " " and (i == 0 or text[i - 1] in ".?:"):
-            i += 1
+    skip_space = False
+
+    for char in text:
+        if skip_space and char == " ":
             continue
+
+        skip_space = False
         print(char, end="")
-        if char in ".?:" and i + 1 < n:
-            print("\n")
-        i += 1
+
+        if char in ".?:":
+            print("\n", end="")
+            skip_space = True
