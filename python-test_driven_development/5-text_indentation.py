1786ca01506e1f0ea9c9889656212834c5777a57
RahafN1 <rahafalabdalh24@gmail.com>
2026-06-13 18:23:12 +0300

    Fix extra newline using end parameter

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -7,16 +7,13 @@ Module that prints text with 2 new lines after ., ? and :
 def text_indentation(text):
     """
     Prints a text with 2 new lines after each ., ? and :
-
     Args:
         text: the text to print
-
     Raises:
         TypeError: if text is not a string
     """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-
     n = len(text)
     i = 0
     while i < n:
@@ -26,5 +23,5 @@ def text_indentation(text):
             continue
         print(char, end="")
         if char in ".?:" and i + 1 < n:
-            print("\n")
+            print("\n", end="")
         i += 1
