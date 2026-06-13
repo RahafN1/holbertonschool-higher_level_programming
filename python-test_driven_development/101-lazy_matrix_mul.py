98e23aac556708b03ed1045284cadeef725357a6
RahafN1 <rahafalabdalh24@gmail.com>
2026-06-13 17:54:38 +0300

    Revert to np.matmul for lazy_matrix_mul

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,19 +1,11 @@
 #!/usr/bin/python3
-"""
-Module for matrix multiplication using NumPy.
-"""
+"""Module that multiplies two matrices using NumPy"""
+
+
 import numpy as np
 
 
 def lazy_matrix_mul(m_a, m_b):
-    """
-    Multiplies two matrices using NumPy.
+    """Multiply two matrices"""
 
-    Args:
-        m_a: first matrix
-        m_b: second matrix
-
-    Returns:
-        numpy.ndarray: product of m_a and m_b
-    """
-    return np.matmul(m_a, m_b)
+    return np.matmul(np.array(m_a), np.array(m_b))
