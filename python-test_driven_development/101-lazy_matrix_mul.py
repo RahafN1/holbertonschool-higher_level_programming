7098958e694dbcd185bb8e400ce4e735f1d66e77
RahafN1 <rahafalabdalh24@gmail.com>
2026-06-13 18:06:01 +0300

    Check shape for scalar detection in lazy_matrix_mul

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,27 +1,16 @@
 #!/usr/bin/python3
-"""Module that multiplies two matrices using NumPy"""
-
+"""
+Module for matrix multiplication using NumPy.
+"""
 import numpy as np
 
 
 def lazy_matrix_mul(m_a, m_b):
-    """Multiply two matrices using NumPy"""
-
-    try:
-        return np.matmul(m_a, m_b)
-    except ValueError:
-        a = np.array(m_a)
-        b = np.array(m_b)
-
-        if len(a.shape) == 2 and len(b.shape) == 2:
-            raise ValueError(
-                "shapes {} and {} not aligned: {} (dim 1) != {} (dim 0)"
-                .format(
-                    a.shape,
-                    b.shape,
-                    a.shape[1],
-                    b.shape[0]
-                )
-            )
-
-        raise
+    """
+    Multiplies two matrices using NumPy.
+    """
+    a = np.array(m_a)
+    b = np.array(m_b)
+    if a.shape == () or b.shape == ():
+        raise ValueError("Scalar operands are not allowed, use '*' instead")
+    return np.matmul(a, b)
