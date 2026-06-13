c508198e2eb7c56c1494a6a0d572609c15d005ae
RahafN1 <rahafalabdalh24@gmail.com>
2026-06-13 17:42:24 +0300

    Fix empty string case in text_indentation

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,23 +1,33 @@
 #!/usr/bin/python3
-"""Module that prints text with 2 new lines after '.', '?' and ':'"""
+"""
+Module that prints text with 2 new lines after ., ? and :
+"""
 
 
 def text_indentation(text):
-    """Print text with 2 new lines after '.', '?' and ':'"""
+    """
+    Prints a text with 2 new lines after each ., ? and :
 
+    Args:
+        text: the text to print
+
+    Raises:
+        TypeError: if text is not a string
+    """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    new_line = True
-
-    for char in text:
-        if char == " " and new_line:
+    n = len(text)
+    if n == 0:
+        return
+    i = 0
+    while i < n:
+        char = text[i]
+        if char == " " and (i == 0 or text[i - 1] in ".?:"):
+            i += 1
             continue
-
         print(char, end="")
-
-        if char in ".?:":
-            print("\n", end="")
-            new_line = True
-        else:
-            new_line = False
+        if char in ".?:" and i + 1 < n:
+            print("\n")
+        i += 1
+    print()
