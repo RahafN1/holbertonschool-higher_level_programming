c629fafab2dbf731bb6b2cf3add7c47215af3fa4
RahafN1 <rahafalabdalh24@gmail.com>
2026-06-13 18:09:23 +0300

    Handle jagged array error message for lazy_matrix_mul

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -9,10 +9,18 @@ def lazy_matrix_mul(m_a, m_b):
     """
     Multiplies two matrices using NumPy.
     """
-    a = np.array(m_a)
-    b = np.array(m_b)
+    try:
+        a = np.array(m_a)
+        b = np.array(m_b)
+    except ValueError:
+        raise ValueError("setting an array element with a sequence.")
+
+    if a.dtype == object or b.dtype == object:
+        raise ValueError("setting an array element with a sequence.")
+
     if a.shape == () or b.shape == ():
         raise ValueError("Scalar operands are not allowed, use '*' instead")
+
     if a.ndim != 2 or b.ndim != 2 or a.shape[1] != b.shape[0]:
         return np.dot(a, b)
     return np.einsum('ij,jk->ik', a, b)
