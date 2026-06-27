be8c5affa870df0d6ac495d0f314f6fe79f5354a
RahafN1 <rahafalabdalh24@gmail.com>
2026-06-27 21:20:39 +0300

    Add Animal abstract class with Dog and Cat subclasses

--- task_00_abc.py
+++ task_00_abc.py
@@ -1,28 +1,18 @@
-#!/usr/bin/python3
-"""Module that defines Animal abstract class and its subclasses"""
+#!/usr/bin/env python3
 from abc import ABC, abstractmethod
 
 
 class Animal(ABC):
-    """Abstract class Animal with abstract method sound"""
-
     @abstractmethod
     def sound(self):
-        """Abstract method that must be implemented by subclasses"""
         pass
 
 
 class Dog(Animal):
-    """Dog class that inherits from Animal"""
-
     def sound(self):
-        """Returns the sound a dog makes"""
         return "Bark"
 
 
 class Cat(Animal):
-    """Cat class that inherits from Animal"""
-
     def sound(self):
-        """Returns the sound a cat makes"""
         return "Meow"
