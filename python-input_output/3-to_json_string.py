0841a18e32ce45e83e6e4262bc5febce101f1d38
RahafN1 <rahafalabdalh24@gmail.com>
2026-07-04 18:15:35 +0300

    Add 3-to_json_string.py: return JSON representation of an object

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module that defines a function to return the JSON representation
+of an object.
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """Return the JSON string representation of an object."""
+    return json.dumps(my_obj)
