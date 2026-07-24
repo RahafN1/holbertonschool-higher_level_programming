16a4b3d291c49b2fd55c3deda2a684d622b6612b
RahafN1 <rahafalabdalh24@gmail.com>
2026-07-24 12:11:57 +0300

    Add 15-comedy_only.sql: lists all Comedy shows sorted by title

--- SQL_more_queries/5-comedy_only.sql
+++ SQL_more_queries/5-comedy_only.sql
@@ -1,7 +1,7 @@
--- Lists all shows in hbtn_0d_tvshows linked to the Comedy genre, sorted by title
+-- Import the database dump from hbtn_0d_tvshows to your MySQL server: download (same as 14-my_genres.sql)
 SELECT tv_shows.title
 FROM tv_shows
-INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
-WHERE tv_genres.name = "Comedy"
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_genres.name = 'Comedy'
 ORDER BY tv_shows.title ASC;
