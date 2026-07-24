562afdeb46ed26224c629a1c6b7cdb41b42f59d4
RahafN1 <rahafalabdalh24@gmail.com>
2026-07-24 11:53:40 +0300

    Add 6-states.sql: creates hbtn_0d_usa db and states table with auto-increment id

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,9 @@
+-- Creates the database hbtn_0d_usa and the table states with auto-incrementing primary key id
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+USE hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS states (
+    id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
+);
