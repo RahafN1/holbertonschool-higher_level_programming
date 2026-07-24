40eba8d67133d548c0f6a414b2a99c5195fb028a
RahafN1 <rahafalabdalh24@gmail.com>
2026-07-25 01:49:47 +0300

    Add 8-model_state_fetch_first.py: prints first State via SQLAlchemy without fetching all

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,29 +1,23 @@
 #!/usr/bin/python3
-"""Script that prints the first State object from the database hbtn_0e_6_usa."""
+"""
+Prints the first State object from the database hbtn_0e_6_usa
+"""
 import sys
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-
     engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'.format(
-            username, password, db_name
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
         ),
         pool_pre_ping=True
     )
-    Base.metadata.create_all(engine)
-
     Session = sessionmaker(bind=engine)
     session = Session()
 
     first_state = session.query(State).order_by(State.id).first()
-
     if first_state is None:
         print("Nothing")
     else:
