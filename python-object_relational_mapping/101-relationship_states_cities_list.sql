7826caf753feed254b2295fd9d2d9768d97d5c6f
RahafN1 <rahafalabdalh24@gmail.com>
2026-08-08 16:45:29 +0300

    rename file

--- python-object_relational_mapping/101-relationship_states_cities_list.sql
+++ python-object_relational_mapping/101-relationship_states_cities_list.sql
@@ -1,31 +1,24 @@
 #!/usr/bin/python3
-"""Script that lists all State objects and their City objects."""
+"""Lists all State objects and corresponding City objects"""
+
 import sys
-from relationship_state import Base, State
-from relationship_city import City
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, joinedload
+from sqlalchemy.orm import sessionmaker
+from relationship_state import Base, State
 
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-
     engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'.format(
-            username, password, db_name
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
         ),
         pool_pre_ping=True
     )
-    Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states = session.query(State).options(
-        joinedload(State.cities)
-    ).order_by(State.id).all()
+    states = session.query(State).order_by(State.id).all()
 
     for state in states:
         print("{}: {}".format(state.id, state.name))
