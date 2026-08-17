77db5490f84761f46b20a99df0312af526f98ad2
RahafN1 <rahafalabdalh24@gmail.com>
2026-08-17 12:41:35 +0300

    add file

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+"""Module that defines a LockedClass with restricted instance attributes."""
+
+
 class LockedClass:
-    __slots__ = ["first_name"]
+    """Class that prevents dynamic creation of new instance attributes,
+    except for 'first_name'.
+    """
+    __slots__ = ['first_name']
